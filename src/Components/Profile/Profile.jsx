8a0cc36363ec19894a31dba734b7d0725861c800
KrishnaKalra <krishna26kalra@gmail.com>
2024-09-25 20:49:53 +0530

    Profile Improvement

--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
-import { useState,useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import DefaultPfp from '../../assets/default.jpg'
 import DivOrigami from '../LogoAnimation/LogoAnimation.jsx'
 import axios from 'axios';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faPen} from '@fortawesome/free-solid-svg-icons'
 function Login() {
   const [img,setImg]=useState();
   const [loading,setLoad]=useState(false);
@@ -28,7 +30,8 @@ function Login() {
     let id=user.email;
     id=id.slice(0,7);
     id=id.toUpperCase();
-    const result=await axios.get(`https://cse-chapter-28-server.vercel.app/api/2027/id?id=${id}`);
+    let year="20"+((Number)(user.email.slice(2,4))+4);
+    const result=await axios.get(`https://cse-chapter-28-server.vercel.app/api/${year}/id?id=${id}`);
     const dt=result.data;
     setData(dt);
     console.log(result.data);
@@ -47,115 +50,119 @@ function Login() {
   }
   return (
     <div >
-    {loading ? (<div className='md:text-xl lg:text-2xl lg:mt-8'>
-      <form method='POST' action='https://cse-chapter-28-server.vercel.app/api/2027/profile'>
+      {loading ? (<div className='md:text-xl lg:text-2xl lg:mt-8'>
+        <form method='POST' action='https://cse-chapter-28-server.vercel.app/api/2027/profile'>
 
-        <div className='flex justify-center h-[100%] w-[100%] mx-auto
-      lg:flex-row flex-col items-center'>
+          <div className='flex justify-center h-[100%] w-[100%] mx-auto
+        lg:flex-row flex-col items-center lg:px-[10%]'>
 
-          <div className='sm:w-[40%] md:w-[50%] w-[80%] items-center mx-auto lg:ml-36  md:ml-32'>
+            <div className='sm:w-[40%] md:w-[50%] w-[80%] flex flex-col items-center'>
 
-            <div className='h-[100%] w-[50%] md:ml-44 lg:mx-auto mx-auto rounded-full'>
-              <label htmlFor="upload" className='lg:w-[300px] lg:h-[300px] md:w-[250px] md:h-[200px]
-                  w-[145px] h-[145px]  rounded-full'>
-                <img
-                  className='lg:w-[300px] lg:h-[300px] md:w-[250px] md:h-[200px]
-                  w-[145px] h-[145px] 
-                  rounded-full object-fill mx-auto hover:opacity-80 cursor-pointer'
-                  src={img ? img : DefaultPfp}
+              <div className='h-[100%] rounded-full relative'>
+              <img
+                    className='lg:w-[300px] aspect-square lg:h-[300px] border-8 border-[#002f26] md:w-[250px] md:h-[250px]
+                    w-[225px] h-[225px] 
+                    rounded-full object-fill mx-auto hover:opacity-80 cursor-pointer'
+                    
+                    src={img ? img : DefaultPfp}
                 />
-              </label>
+                <div className='bg-[#002f26] rounded-full h-[4.5rem] w-[4.5rem] absolute right-0 bottom-0'>
+                <label htmlFor='upload' className='h-[4.5rem] w-[4.5rem] rounded-full flex items-center justify-center'>
+                <FontAwesomeIcon icon={faPen} className='h-[1.5rem] w-[1.5rem]' style={{color: "#ffffff",}} />
+                </label>
+                <input
+                  className='[4.5rem] w-[4.5rem] rounded-full bg-transparent opacity-0'
+                  id='upload'
+                  hidden
+                  type='file' onChange={ImageInput}>
+                </input>
+                </div>
+              </div>
+
+              <input
+                className='w-[90%] lg:mx-auto md:ml-20 bg-white bg-opacity-0 p-2 mt-6 rounded-lg font-extrabold text-center text-white text-3xl uppercase'
+                readOnly type='text' name='name' placeholder="Name" value={data[0].name}></input>
+
+              <input
+                className='w-[100%] bg-white bg-opacity-0 p-2  rounded-lg font-extrabold text-center text-white'
+                readOnly hidden type='text' name='id' placeholder="Id" value={data[0].id}></input>
+
+              <div className=' w-[90%] flex justify-center '>
+                <img
+                  className='inline w-10 h-10 mr-3'
+                  src="https://img.icons8.com/material-sharp/48/marker.png" alt="marker" />
+                <input
+                  className='w-[75%] lg:w-[90%] mr-10 bg-white bg-opacity-20 backdrop-blur-sm p-2 pl-3  rounded-lg max-w-[86%]  placeholder-black'
+                  type='text' name='location' placeholder={(data[0].Location)?data[0].Location:'Location'}>
+                </input>
+              </div>
+            </div>
+
+            <div className=' sm:w-[50%] w-[80%] h-[70%]'>
+
+              <br />
+              <h1 className=' font-extrabold  text-4xl text-center'>About me</h1>
+              <textarea
+                className=' w-[100%] bg-white h-[30%] bg-opacity-20 xl:max-w-3xl backdrop-blur-sm pt-3 
+                 mt-6 rounded-lg max-w-2xl  placeholder-black resize-none p-4' rows='4'
+                name='description' placeholder={data[0].Description?data[0].Description:'Description'}>
+              </textarea>
+              <br />
+              <br />
+
+              <div className='w-[100%] flex mt-5'>
+                <img 
+                  className='inline mx-4 size-12 '
+                  src="https://img.icons8.com/glyph-neue/64/instagram-new--v1.png" alt="instagram-new--v1" />
+                <input
+                  className='w-[89.25%]  bg-white bg-opacity-20 backdrop-blur-sm p-2 pl-3  rounded-lg  placeholder-black'
+                  type='text' name='instagram' placeholder={(data[0].Instagram)?data[0].Instagram:'Instagram'}></input>
+              </div>
+
+              <div className='w-[100%] flex mt-5'>
+                <img
+                  className='inline mx-4 size-12'
+                  src="https://img.icons8.com/ios-filled/50/linkedin.png" alt="linkedin" />
+                <input
+                  className='w-[89.25%] bg-white bg-opacity-20 backdrop-blur-sm p-2 pl-3  rounded-lg placeholder-black'
+                  type='text' name='linkedin' placeholder={(data[0].LinkedIn)?data[0].LinkedIn:'LinkedIn'}></input>
+              </div>
+
+              <div className='w-[100%] flex mt-5'>
+                <img 
+                  className='inline mx-4 mb-[2px] size-12'
+                  src="https://img.icons8.com/glyph-neue/64/github.png" alt="github" />
+                <input
+                  className='w-[89.25%] bg-white bg-opacity-20 backdrop-blur-sm p-2 pl-3  rounded-lg  placeholder-black'
+                  type='text' name='github' placeholder={(data[0].GitHub)?data[0].GitHub:'GitHub'}></input>
+              </div>
+              <br />
+              <br />
+
+
+
+              <input type='text' hidden name="image" value={img} />
+            </div>
+          </div>
+          <div >
+            <button
+              className='
+              hover:border-black border-[1px]
+              md:ml-[45vw] ml-[35vw] px-8 py-2 lg:mt-6 
+              rounded-lg bg-[#B2D7D0] font-extrabold md:text-2xl self-center'
+              type="submit">
+              SUBMIT
               <input
                 className='hidden'
-                id='upload'
-                type='file' onChange={ImageInput}>
-              </input>
-            </div>
-
-            <input
-              className='w-[100%] lg:mx-auto md:ml-20 bg-white bg-opacity-0 p-2 mt-6 rounded-lg font-extrabold text-center text-white text-3xl uppercase'
-              readOnly type='text' name='name' placeholder="Name" value={data[0].name}></input>
-
-            <input
-              className='w-[100%] bg-white bg-opacity-0 p-2  rounded-lg font-extrabold text-center text-white'
-              readOnly hidden type='text' name='id' placeholder="Id" value={data[0].id}></input>
-
-            <div className=' w-[100%] md:ml-24 mx-auto'>
-              <img
-                width="32" height="32"
-                className='inline mx-4 '
-                src="https://img.icons8.com/material-sharp/48/marker.png" alt="marker" />
-              <input
-                className='w-[75%] lg:w-[60%] bg-white bg-opacity-20 backdrop-blur-sm p-2 pl-3 mt-6 rounded-lg max-w-[86%]  placeholder-black'
-                type='text' name='location' placeholder="Location" value={data[0].Location}>
-              </input>
-            </div>
+                type='submit' />
+            </button>
           </div>
+        </form>
+        <br />
 
-          <div className=' sm:w-[40%] w-[80%] mx-auto lg:mr-32'>
-
-            <br />
-            <h1 className=' font-extrabold  text-4xl text-center'>About me</h1>
-            <textarea
-              className='sm:h-[100%] w-[100%] bg-white bg-opacity-20 xl:max-w-3xl backdrop-blur-sm p-20 pt-3 pl-4 mt-6 rounded-lg max-w-2xl xl:ml-6 placeholder-black resize-none max-h-[40%]'
-              name='description' placeholder='Description' value={data[0].Description}>
-            </textarea>
-            <br />
-            <br />
-
-            <div className='w-[100%]'>
-              <img width="32" height="32"
-                className='inline mx-4'
-                src="https://img.icons8.com/glyph-neue/64/instagram-new--v1.png" alt="instagram-new--v1" />
-              <input
-                className='w-[75%]  xl:w-[100%] bg-white bg-opacity-20 backdrop-blur-sm p-2 pl-3 mt-6 rounded-lg max-w-[86%]  placeholder-black'
-                type='text' name='instagram' placeholder='Instagram' value={data[0].Instagram}></input>
-            </div>
-
-            <div className='w-[100%]'>
-              <img width="32" height="32"
-                className='inline mx-4'
-                src="https://img.icons8.com/ios-filled/50/linkedin.png" alt="linkedin" />
-              <input
-                className='w-[75%]  xl:w-[100%] bg-white bg-opacity-20 backdrop-blur-sm p-2 pl-3 mt-6 rounded-lg max-w-[86%]  placeholder-black'
-                type='text' name='linkedin' placeholder='LinkedIn' value={data[0].LinkedIn}></input>
-            </div>
-
-            <div className='w-[100%]'>
-              <img width="32" height="32"
-                className='inline mx-4 mb-[2px]'
-                src="https://img.icons8.com/glyph-neue/64/github.png" alt="github" />
-              <input
-                className='w-[75%]  xl:w-[100%] bg-white bg-opacity-20 backdrop-blur-sm p-2 pl-3 mt-6 rounded-lg max-w-[86%]  placeholder-black'
-                type='text' name='github' placeholder='GitHub' value={data[0].GitHub}></input>
-            </div>
-            <br />
-            <br />
-
-
-
-            <input type='text' hidden name="image" value={img} />
-          </div>
-        </div>
-        <div >
-          <button
-            className='
-            hover:border-black border-[1px]
-            md:ml-[45vw] ml-[35vw] px-8 py-2 lg:mt-6 
-            rounded-lg bg-[#B2D7D0] font-extrabold md:text-2xl self-center'
-            type="submit">
-            SUBMIT
-            <input
-              className='hidden'
-              type='submit' />
-          </button>
-        </div>
-      </form>
-      <br />
-
-    </div>) : (<DivOrigami />)
-    }
-  </div>
+      </div>) : (<DivOrigami />)
+      }
+    </div>
   )
 }
 

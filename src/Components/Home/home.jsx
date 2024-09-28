74c9ce42b37047c30a670809be6532cc2bc62188
Kohinoor <kohinoor2004@outlook.com>
2024-09-29 03:15:17 +0530

    hwll

--- src/Components/Home/home.jsx
+++ src/Components/Home/home.jsx
@@ -19,6 +19,7 @@ function home() {
           <h2 class="text-3xl text-center solwa stroke font-semibold">A hearty welcome to the CSE chapter '28...</h2>
           <p class="text-2xl font-semibold solway">
             This bootcamp is designed to spark your excitement and curiosity about knowing your seniors and about the college life which offers a vibrant, interactive glimpse into what awaits you. This highlights the exciting side of college life, featuring fun events, college fests, inspiring student stories, and opportunities. Our team has poured their hearts into making this website to give you a genuine glimpse into college life and help you feel excited about your future here. We hope you enjoy exploring it as much as we enjoyed making it for you!!!
+            This is a changed text
           </p>
         </div>
              

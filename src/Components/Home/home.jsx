77bea9944a7e38541b2d68b60e28aee94637b0bc
partha120804 <138643473+partha120804@users.noreply.github.com>
2024-09-29 00:47:40 +0530

    commit

--- src/Components/Home/home.jsx
+++ src/Components/Home/home.jsx
@@ -17,7 +17,7 @@ function home() {
         <img className=' mt-4 h-[5vh] lg:h-[20vh] w-[85vw] lg:w-[55vw]' src={welcome}/>
         </div>
           <h2 class="text-center text-3xl sofia stroke font-semibold lg:text-4xl">A HEARTY WELCOME TO THE CSE CHAPTER '28</h2>
-          <p class=" font-semibold solway lg:text-2xl">
+          <p class=" text-lg font-semibold solway lg:text-2xl">
             This bootcamp is designed to spark your excitement and curiosity about knowing your seniors and about the college life which offers a vibrant, interactive glimpse into what awaits you. This highlights the exciting side of college life, featuring fun events, college fests, inspiring student stories, and opportunities. Our team has poured their hearts into making this website to give you a genuine glimpse into college life and help you feel excited about your future here. We hope you enjoy exploring it as much as we enjoyed making it for you!!!
           </p>
         </div>

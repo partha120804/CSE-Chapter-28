9a40bdee85169a84944019cf8ff8d9553b6a262d
partha120804 <138643473+partha120804@users.noreply.github.com>
2024-09-30 21:04:07 +0530

    done

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -21,11 +21,11 @@ function about() {
   return (
     <div className="flex justify-center items-center  h-[100%] flex-col">
     <div className="text-center mt-5 mb-[60px]">
-    <h1 className=" text-8xl lg:text-9xl mb-4 mr-[10%] ml-[10%] font-bold text-[#002F26] sofia">
+    <h1 className=" text-6xl lg:text-9xl mb-4 mr-[10%] ml-[10%] font-bold text-[#002F26] sofia">
   <span>ABOUT US</span>
 </h1>
 <h1 className=" text-[#d0f0ea] ml-[10%] mr-[10%] flex flex-col items-center justify-center gap-5 text-justify">
-<p className="text-4xl sofia lg:text-5xl text-center text-[#004040]">Welcome to CSE Chapter '28</p>
+<p className="text-3xl sofia lg:text-5xl text-center text-[#004040]">Welcome to CSE Chapter '28</p>
       <p className="text-lg poppins tracking-[0.018em] lg:text-3xl">
 Crafted with passion and dedication, this website is the result of hard work and creativity from our team.
 We hope this platform serves as a helpful resource for staying connected, informed, and inspired and you find it as exciting and useful as we envisioned!!!
@@ -33,7 +33,7 @@ We hope this platform serves as a helpful resource for staying connected, inform
 </h1>
     </div>
 <div className=' flex flex-col justify-center items-center mb-[90px]'>
-  <h1 className='text-7xl lg:text-8xl mb-6 mr-[10%] ml-[10%] font-semibold text-white sofia'>Our Team</h1>
+  <h1 className='text-5xl lg:text-8xl mb-6 mr-[10%] ml-[10%] font-semibold text-white sofia'>Our Team</h1>
     <div className="cards flex-wrap flex flex-row justify-center gap-10 max-w-[80vw]">
       <AboutCard named="Krishna Kalra" 
         pic={krishna}
@@ -101,7 +101,7 @@ We hope this platform serves as a helpful resource for staying connected, inform
       />
     </div>
 </div>
-<h1 className='text-5xl lg:text-7xl mb-5 mr-[10%] ml-[10%] text-white sofia'>Tech Stack Used</h1>
+<h1 className='text-4xl lg:text-7xl mb-5 mr-[10%] ml-[10%] text-white sofia'>Tech Stack Used</h1>
 <div className='mr-[10%] ml-[10%] mb-10 flex flex-row justify-between align-middle text-lg gap-[1em] lg:text-4xl flex-nowrap lg:gap-[1.5em] text-[#d0f0ea]'>
   <SiReact title="React"/>
   <SiHtml5 title="HTML 5"/>
